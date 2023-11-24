a8295a0a8f8084ce3f5711c5324595fe49e67c0f
patrictobola <tobola92@gmail.com>
2023-11-24 15:34:15 +0100

    milestone3

--- src/main/java/db_nation/Main.java
+++ src/main/java/db_nation/Main.java
@@ -2,18 +2,23 @@ package db_nation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/db_nation";
         String user = "root";
         String password = "root";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Cerca una parola: ");
+            String searchString = scanner.nextLine();
+
             String query = "SELECT " +
                     "countries.country_id AS id, " +
                     "countries.name AS name, " +
@@ -22,22 +27,25 @@ public class Main {
                     "FROM countries " +
                     "JOIN regions ON countries.region_id = regions.region_id " +
                     "JOIN continents ON regions.continent_id = continents.continent_id " +
+                    "WHERE countries.name LIKE ? " +
                     "ORDER BY countries.name";
 
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(query)) {
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String name = resultSet.getString("name");
-                    String region = resultSet.getString("region");
-                    String continent = resultSet.getString("continent");
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, "%" + searchString + "%");
 
-                    System.out.println("ID: " + id + ", Name: " + name + ", Region: " + region + ", Continent: " + continent);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt("id");
+                        String name = resultSet.getString("name");
+                        String region = resultSet.getString("region");
+                        String continent = resultSet.getString("continent");
+
+                        System.out.println("ID: " + id + ", Name: " + name + ", Region: " + region + ", Continent: " + continent);
+                    }
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
 }
